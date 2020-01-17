0baebf37bb4b43d46a88c5f0e66cc7d0991bd727
Chirag Modi <mchirag1247@gmail.com>
2020-01-17 18:22:03 +0530

    4 routers added

---
+++ vueJs/myvueapp/src/views/Chirag.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h3>Chirag Modi</h3>
+        <p>Contact: 8320369696</p>
+    </div>
+</template>
---
+++ vueJs/myvueapp/src/views/Contact.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <input type="email"><br>
+        <button>Subscribe</button>
+    </div>
+</template>
