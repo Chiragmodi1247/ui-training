ebb795b705ac8f9e396cd15cc10fb2661538a1ed
Chirag Modi <mchirag1247@gmail.com>
2020-01-17 17:06:35 +0530

    before router

---
+++ vueJs/myvueapp/src/components/Chirag.vue
@@ -0,0 +1,38 @@
+<template>
+  <h3>
+    Hi, Chirag component is working.
+    <br />
+    The name is {{usernamerefactored}} and age is {{age}} and it's double is {{ doubleOfNumber }}
+    <button
+      @click="increaseFavNumber"
+    >Increase</button>
+  </h3>
+</template>
+
+<script>
+export default {
+  name: "Chirag",
+  data: () => {
+    return {
+      name: "Chirag_modi",
+    };
+  },
+  props:[''],
+  computed: {
+    doubleOfNumber() {
+      return this.age*2
+    },
+    usernamerefactored() {
+      return this.name.split('_').join(' ');
+    }    
+  },
+  methods: {
+    increaseFavNumber: function() {
+      this.age++;
+    }
+  }
+};
+</script>
+
+<style>
+</style>
